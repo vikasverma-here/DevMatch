7fa3fa04575f26029618584a558c250b42055486
vikasverma-here <vikasverma995619@gmail.com>
2024-12-05 08:44:32 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express= require('express')
+const app = express()
+app.use("/test", (req,res)=>{
+    res.send("passing some test cases")
+})
+app.use("/home", (req,res)=>{
+    res.send("home always home")
+})
+
+app.use("/about", (req,res)=>{
+    res.send("about ")
+})
+app.use("/content", (req,res)=>{
+    res.send("content change  ")
+})
+app.use( (req,res)=>{
+    res.send("nothing")
+})
+app.listen(7777,()=>{
+    console.log("if server is listening succesfully")
+})
